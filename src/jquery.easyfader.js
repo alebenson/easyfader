04e482b94a7776612338e38061e46dcb706c2949
Patrick Kunka <barrelimac21@Barrel-iMac-21s-iMac.local>
2013-08-09 13:33:11 -0400

    Complete Object-Oriented Re-write

--- src/jquery.easyfader.js
+++ src/jquery.easyfader.js
@@ -1,6 +1,6 @@
 /*
 * EASYFADER - An Ultralight Fading Slideshow For Responsive Layouts
-* Version: 1.4
+* Version: 1.5
 * License: Creative Commons Attribution 3.0 Unported - CC BY 3.0
 * http://creativecommons.org/licenses/by/3.0/
 * This software may be used freely on commercial and non-commercial projects with attribution to the author/copyright holder.
@@ -9,15 +9,6 @@
 */
 
 (function($){
-	function prefix(el){
-		var prefixes = ["Webkit", "Moz", "O", "ms"];
-		for (var i = 0; i < prefixes.length; i++){
-			if (prefixes[i] + "Transition" in el.style){
-				return '-'+prefixes[i].toLowerCase()+'-'; 
-			};
-		}; 
-		return "transition" in el.style ? "" : false;
-	};
 	$.fn.removeStyle = function(style){
 		return this.each(function(){
 			var obj = $(this);
@@ -29,110 +20,142 @@
 					if(style) return style.replace(search, '');
 			    });
 			});
-		});
-	};
-	var methods = {
-		init: function(settings){
-				return this.each(function(){
-					var self = this,
-						config = {
-							slideDur: 7000,
-							fadeDur: 800,
-							onFadeStart: null,
-							onFadeEnd: null,
-							$container: $(self),
-							slideSelector: '.slide',
-							fading: false,
-							firstLoad: true,
-							slideTimer: null,
-							activeSlide: null,
-							newSlide: null,
-							$slides: null,
-							totalSlides: null,
-							$pagerList: null,
-							$pagers: null
-						};
-					if(settings){
-						$.extend(config, settings);
-					};
-					config.$slides = config.$container.find(config.slideSelector);
-					config.totalSlides = config.$slides.length;
-					config.$pagerList = config.$container.find('.pager_list');
-					config.prefix = $.support.leadingWhitespace ? prefix(config.$container[0]) : false;
-					for(var i = 0; i < config.totalSlides; i++){
-						config.$pagerList
-							.append('<li class="pager" data-target="'+i+'">'+(i+1)+'</li>');
-					};
-					config.$container.find('.pager').on('click',function(){
-						var target = $(this).attr('data-target');
-						clearTimeout(config.slideTimer);
-						methods.changeSlides(config, target);
-					});
-					config.$pagers = config.$pagerList.find('.pager');
-					config.$pagers.eq(0).addClass('active');
-					methods.animateSlides(config, 1, 0);
-				});
-			},
-			cleanUp: function(config, activeNdx, newNdx){
-				config.$slides.eq(activeNdx).removeStyle('opacity, z-index');
-				config.$slides.eq(newNdx).removeStyle(config.prefix+'transition, transition');
-				config.activeSlide = newNdx;
-				config.fading = false;
-				methods.waitForNext(config);
-				if(typeof config.onFadeEnd == 'function'){
-					config.onFadeEnd.call(this, config.$slides.eq(config.activeSlide));
+			if(typeof obj.attr('style') !== 'undefined'){
+				var cleanStyle = obj.attr('style').replace(/\s{2,}/g, ' ').trim();
+				obj.attr('style', cleanStyle);
+				if(cleanStyle == ''){
+					obj.removeAttr('style');
 				};
-			},
-			animateSlides: function(config, activeNdx, newNdx){
-				if(config.fading || activeNdx == newNdx){
-					return false;
-				};
-				config.fading = true;
-				if(typeof config.onFadeStart == 'function' && !config.firstLoad){
-					config.onFadeStart.call(this, config.$slides.eq(config.newSlide));
-				};
-				config.$pagers.removeClass('active').eq(config.newSlide).addClass('active');
-				config.$slides.eq(activeNdx).css('z-index', 2);
-				config.$slides.eq(newNdx).css('z-index', 3);
-				if(!config.prefix){
-					config.$slides.eq(newNdx).animate({'opacity': 1}, config.fadeDur,
-					function(){
-						methods.cleanUp(config, activeNdx, newNdx);
-					});
-				} else {
-					var styles = {};
-					styles[config.prefix+'transition'] = 'opacity '+config.fadeDur+'ms';
-					styles['opacity'] = 1;
-					config.$slides.eq(newNdx).css(styles);
-					var fadeTimer = setTimeout(function(){
-						methods.cleanUp(config, activeNdx, newNdx);
-					},config.fadeDur);
-				};
-			},
-			changeSlides: function(config, target){
-				if(target == 'next'){
-					config.newSlide = config.activeSlide + 1;
-					if(config.newSlide > config.totalSlides - 1){
-						config.newSlide = 0;
-					}
-				} else if(target == 'prev'){
-					config.newSlide = config.activeSlide - 1;
-					if(config.newSlide < 0){
-						config.newSlide = config.totalSlides - 1;
-					};
-				} else {
-					config.newSlide = target;
-				};
-				methods.animateSlides(config, config.activeSlide, config.newSlide);
-			},
-			waitForNext: function(config){
-				config.firstLoad = false;
-				config.slideTimer = setTimeout(function(){
-					methods.changeSlides(config, 'next');
-				},config.slideDur);
 			}
-		};
+		});
+    };
+
+	function prefix(el){
+		var prefixes = ["Webkit", "Moz", "O", "ms"];
+		for (var i = 0; i < prefixes.length; i++){
+			if (prefixes[i] + "Transition" in el.style){
+				return '-'+prefixes[i].toLowerCase()+'-'; 
+			};
+		}; 
+		return "transition" in el.style ? "" : false;
+	};
+
+	function EasyFader(){
+		this.slideDur = 7000,
+		this.fadeDur = 800,
+		this.onFadeStart = null,
+		this.onFadeEnd = null,
+		this.slideSelector = '.slide',
+		this.fading = false,
+		this.firstLoad = true,
+		this.slideTimer = null,
+		this.activeSlide = null,
+		this.newSlide = null,
+		this.$slides = null,
+		this.totalSlides = null,
+		this.$pagerList = null,
+		this.$pagers = null;
+	};
+	
+	EasyFader.prototype = {
+		constructor: EasyFader,
+		init: function(domNode, settings){
+			var self = this;
+			
+			if(settings){
+				$.extend(self, settings);
+			};
+			self.$container = $(domNode);
+			
+			self.$slides = self.$container.find(self.slideSelector);
+			self.totalSlides = self.$slides.length;
+			self.$pagerList = self.$container.find('.pager_list');
+			self.prefix = $.support.leadingWhitespace ? prefix(self.$container[0]) : false;
+			for(var i = 0; i < self.totalSlides; i++){
+				self.$pagerList
+					.append('<li class="pager" data-target="'+i+'">'+(i+1)+'</li>');
+			};
+			self.$container.find('.pager').on('click',function(){
+				var target = $(this).attr('data-target');
+				clearTimeout(self.slideTimer);
+				self.changeSlides(target);
+			});
+			self.$pagers = self.$pagerList.find('.pager');
+			self.$pagers.eq(0).addClass('active');
+			self.animateSlides(1, 0);
+		},
+		cleanUp: function(activeNdx, newNdx){
+			var self = this;
+			
+			self.$slides.eq(activeNdx).removeStyle('opacity, z-index');
+			self.$slides.eq(newNdx).removeStyle(self.prefix+'transition, transition');
+			self.activeSlide = newNdx;
+			self.fading = false;
+			self.waitForNext();
+			if(typeof self.onFadeEnd == 'function'){
+				self.onFadeEnd.call(this, self.$slides.eq(self.activeSlide));
+			};
+		},
+		animateSlides: function(activeNdx, newNdx){
+			var self = this;
+			
+			if(self.fading || activeNdx == newNdx){
+				return false;
+			};
+			self.fading = true;
+			if(typeof self.onFadeStart == 'function' && !self.firstLoad){
+				self.onFadeStart.call(this, self.$slides.eq(self.newSlide));
+			};
+			self.$pagers.removeClass('active').eq(self.newSlide).addClass('active');
+			self.$slides.eq(activeNdx).css('z-index', 2);
+			self.$slides.eq(newNdx).css('z-index', 3);
+			if(!self.prefix){
+				self.$slides.eq(newNdx).animate({'opacity': 1}, self.fadeDur,
+				function(){
+					self.cleanUp(activeNdx, newNdx);
+				});
+			} else {
+				var styles = {};
+				styles[self.prefix+'transition'] = 'opacity '+self.fadeDur+'ms';
+				styles['opacity'] = 1;
+				self.$slides.eq(newNdx).css(styles);
+				var fadeTimer = setTimeout(function(){
+					self.cleanUp(activeNdx, newNdx);
+				},self.fadeDur);
+			};
+		},
+		changeSlides: function(target){
+			var self = this;
+			
+			if(target == 'next'){
+				self.newSlide = self.activeSlide + 1;
+				if(self.newSlide > self.totalSlides - 1){
+					self.newSlide = 0;
+				}
+			} else if(target == 'prev'){
+				self.newSlide = self.activeSlide - 1;
+				if(self.newSlide < 0){
+					self.newSlide = self.totalSlides - 1;
+				};
+			} else {
+				self.newSlide = target;
+			};
+			self.animateSlides(self.activeSlide, self.newSlide);
+		},
+		waitForNext: function(){
+			var self = this;
+			
+			self.firstLoad = false;
+			self.slideTimer = setTimeout(function(){
+				self.changeSlides('next');
+			},self.slideDur);
+		}
+	};
+	
 	$.fn.easyFader = function(settings){
-		return methods.init.apply(this, arguments);
+		return this.each(function(){
+			var instance = new EasyFader();
+			instance.init(this, settings);
+		});
 	};
 })(jQuery);
